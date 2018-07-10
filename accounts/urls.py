8cfb3834a1d5d8a646ebc83886143775acaad634
bijan rostamnejad <brostamnejad@ut.ac.ir>
2018-07-10 13:11:15 +0430

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as authView
+from accounts.views import userLogin, signup, register, dashboard, postPublish, getProfile, friendship, breakfriendship
+from django.conf import settings
+from django.conf.urls.static import static
+app_name = 'accounts'
+urlpatterns = [
+    path('login/', userLogin, name = 'login'),
+    # path('login/', authView.LoginView.as_view(), name='logout'),
+    path('logout/', authView.LogoutView.as_view(), name='logout'),
+    path('signup/', register, name = 'signup'),
+    path('dashboard/<username>/',dashboard, name= 'dashboard' ),
+    path('submit/', postPublish, name = 'publish-post'),
+    path('friendship/',friendship,name='friendship'),
+    path('break/', breakfriendship, name='break'),
+    path('profile/<username>/<you>', getProfile, name='profile')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
